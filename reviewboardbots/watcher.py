d1f43a7e759fba6c7b0a29a7dbcb0bc37212f4f4
Zach Brown <zach.brown@ni.com>
2016-08-30 18:17:32 -0500

    New version of polling, a dict is maintained noting all reviews seen before
    This means we will never revaluate a request even if we should

--- reviewboardbots/watcher.py
+++ reviewboardbots/watcher.py
@@ -10,8 +10,9 @@ class Watcher:
         self.client = RBClient(server)
         self.names_of_interest = self.getNamesOfInterest()
         self.bot_manager = BotManager("../bots", "../bot_scripts")
-        time_obj = datetime.datetime.utcnow() - datetime.timedelta(minutes=5)
-        self.newest_request_seen = time_obj.isoformat()
+        self.time_obj = datetime.datetime.utcnow() - datetime.timedelta(minutes=5)
+        self.newest_request_seen = self.time_obj.isoformat()
+        self.requests_seen = {}
 
     def getNamesOfInterest(self):
         """who the watcher should watch for"""
@@ -20,38 +21,43 @@ class Watcher:
 
     def setNewestTimestamp(self,requests):
         """We want to filter requests out from the server, but need to use its timestamps"""
-        """Assume chronological order he,he,he"""
+        time_max = self.time_obj
         for request in requests:
             "Get timestamp from newest request in this group and inc by microsecond :)"
             time_str = request.last_updated
             time_obj = datetime.datetime.strptime(time_str, "%Y-%m-%dT%H:%M:%SZ")
-            time_obj += datetime.timedelta(1)
-            self.newest_request_seen = time_obj.isoformat()
-            break
+            #time_obj += datetime.timedelta(1)
+
+            if time_obj.time() > time_max.time():
+                time_max = time_obj
+
+        if time_max != self.time_obj:
+            self.time_obj = time_max
+            self.time_obj += datetime.timedelta(seconds=1)
+            print "Updating time "  + self.time_obj.isoformat()
+            self.newest_request_seen = self.time_obj.isoformat()
 
 
     def getNewRequests(self):
         """get reviews after the timestamp"""
-        print("Looking for reviews")
         root = self.client.get_root()
         requests = root.get_review_requests(last_updated_from=self.newest_request_seen)
 
-        filtered_requests = Watcher.filterRequests(requests, self.names_of_interest)
+        filtered_requests = self.filterRequests(requests, self.names_of_interest)
 
         "Set the timestamp for next time"
         self.setNewestTimestamp(requests)
-        print self.newest_request_seen
         return filtered_requests
 
-    @staticmethod
-    def filterRequests(requests, watched_names):
+    def filterRequests(self, requests, watched_names):
         filtered_requests = {}
         for request in requests:
             for person in request.target_people:
-                if person.title in watched_names:
+                if person.title in watched_names and request.id not in self.requests_seen:
                    if person.title not in filtered_requests:
                        filtered_requests[person.title] = []
                    filtered_requests[person.title].append(request)
+                   self.requests_seen[request.id] = True
         return filtered_requests
 
     def watch(self):
@@ -59,11 +65,16 @@ class Watcher:
 
         "Until the end of time"
         print("I am watching")
+        count = 0
         while True:
             self.bot_manager.processNewReviews(self.getNewRequests())
 
+            count += 1
+            print "Round " + str(count)
+            """TODO Something is still broken here....:("""
+
             "TODO pick good wait time"
-            time.sleep(1)
+            time.sleep(5)
 
 "Example"
 watcher = Watcher('http://pds-rbdev01')
