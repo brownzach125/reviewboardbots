103c6742e13c045160fb2946500c55467f187119
Zach Brown <zach.brown@ni.com>
2016-08-31 10:20:46 -0500

    Working on improving polling

--- reviewboardbots/watcher.py
+++ reviewboardbots/watcher.py
@@ -3,6 +3,11 @@ from rbtools.api.client import RBClient
 import datetime
 from botmanager import BotManager
 
+
+def parseServerTimeStamp(timestamp):
+    return datetime.datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%SZ")
+
+
 class Watcher:
     """The thing that watches review board for things"""
     def __init__(self,server):
@@ -25,7 +30,7 @@ class Watcher:
         for request in requests:
             "Get timestamp from newest request in this group and inc by microsecond :)"
             time_str = request.last_updated
-            time_obj = datetime.datetime.strptime(time_str, "%Y-%m-%dT%H:%M:%SZ")
+            time_obj = parseServerTimeStamp(time_str)
             #time_obj += datetime.timedelta(1)
 
             if time_obj.time() > time_max.time():
@@ -53,12 +58,33 @@ class Watcher:
         filtered_requests = {}
         for request in requests:
             for person in request.target_people:
-                if person.title in watched_names and request.id not in self.requests_seen:
+                if person.title in watched_names and self.isNew(request):
                    if person.title not in filtered_requests:
                        filtered_requests[person.title] = []
                    filtered_requests[person.title].append(request)
-                   self.requests_seen[request.id] = True
         return filtered_requests
+
+    def isNew(self, request):
+        changes = self.client.get_root().get_changes(review_request_id=request.id)
+
+        "Find the max change timestamp"
+        max = parseServerTimeStamp(request.time_added)
+        for change in changes:
+            time_obj = parseServerTimeStamp(change.timestamp)
+            if time_obj.time() > max.time():
+                max = time_obj
+        if request.id in self.requests_seen:
+            old_time = self.requests_seen[request.id]
+            if max > old_time:
+                "There has been a change since we saw it last"
+                self.requests_seen[request.id] = max
+                return True
+            else:
+                return False
+        else:
+            self.requests_seen[request.id] = max
+            return True
+
 
     def watch(self):
         """Periodically check for new reviews, spin off bots when needed"""
