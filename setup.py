4c546ccbd2331edeed78f966c589465100cf78c8
Matthew Shafer <matthew.shafer@ni.com>
2017-10-13 09:17:59 -0500

    making botfood_folder automatically created
    reformat file to pep8 spacing

--- setup.py
+++ setup.py
@@ -2,14 +2,16 @@ from setuptools import setup
 import os
 
 setup(name='reviewboardbots',
-        version='0.1',
-        description='The best ever review bot system',
-        author='Zach and Tim',
-        install_requires=[
-            'RBTools',
-            'sh',
-            'tinydb', 'pyyaml',
-        ],
-        )
+      version='0.1',
+      description='The best ever review bot system',
+      author='Zach and Tim',
+      install_requires=[
+          'RBTools',
+          'sh',
+          'tinydb', 'pyyaml',
+      ],
+      )
 
-os.mkdir('botfood_folder')
+bot_food_folder = 'botfood_folder'
+if not os.path.isdir(bot_food_folder):  # if folder does not exist,
+    os.mkdir(bot_food_folder)           # create it
